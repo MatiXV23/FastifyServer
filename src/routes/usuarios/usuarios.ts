233d4c14e65a0b52858732fed4b8ac0072d1218d
brahnu <contacto@brahnu.cl>
2025-08-28 19:18:31 -0300

    Put y Delete

--- src/routes/usuarios/usuarios.ts
+++ src/routes/usuarios/usuarios.ts
@@ -115,6 +115,70 @@ async function usuariosRoutes(fastify: FastifyInstance, options: object) {
       return usuario;
     }
   );
+
+
+  fastify.put(
+    "/usuarios/:id_usuario",
+    {
+      schema: {
+        summary: "Modificar usuarios",
+        description: "Esta ruta permite modificar un nuevo usuario.",
+        tags: ["usuarios"],
+        params: {
+        type: "object",
+        properties: {
+          id_usuario: { type: "number", minimum: 1 },
+        },
+        required: ["id_usuario"],
+        },
+        body: usuarioPostSchema,
+        response: {
+            204: usuarioSchema,
+        }
+      } as FastifySchema,
+    },
+    async function handler(req, rep) {
+        const { id_usuario} = req.params as {id_usuario: number}; 
+        const { nombre, isAdmin } = req.body as {nombre: "String", isAdmin: boolean}; 
+        const usuarioIndex = usuarios.findIndex((u)=>u.id_usuario===id_usuario);
+        if(usuarioIndex===-1){
+          return rep.code(404).send({error: "Usuario no encontrado."});
+        }
+        usuarios[usuarioIndex]={nombre, isAdmin, id_usuario};
+      return usuarios[usuarioIndex];
+    }
+  );
+  fastify.delete(
+    "/usuarios/:id_usuario",
+    {
+      schema: {
+        summary: "Eliminar usuarios",
+        description: "Esta ruta permite eliminar un nuevo usuario.",
+        tags: ["usuarios"],
+        params: {
+        type: "object",
+        properties: {
+          id_usuario: { type: "number", minimum: 1 },
+        },
+        required: ["id_usuario"],
+        },
+        response: {
+            204: usuarioSchema,
+        }
+      } as FastifySchema,
+    },
+    async function handler(req, rep) {
+        const { id_usuario} = req.params as {id_usuario: number}; 
+        //const { nombre, isAdmin } = req.body as {nombre: "String", isAdmin: boolean}; 
+        const usuarioIndex = usuarios.findIndex((u)=>u.id_usuario===id_usuario);
+        if (usuarioIndex===-1){
+            return rep.code(404).send({error: "Usuario no encontrado."});
+        }else{
+            usuarios.splice(usuarioIndex,1);
+            return rep.code(204).send("Usuario eliminado con éxito.");//debería mostrar mensaje pero no funciona manito
+        };
+    }
+  );
 }
 
   export default usuariosRoutes;
