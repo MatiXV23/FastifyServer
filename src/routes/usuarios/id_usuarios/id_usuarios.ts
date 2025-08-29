341e52c6b8b7dcbfc969037ab6538feb3b83cf7a
Mati <mperezfrontan@gmail.com>
2025-08-28 22:41:56 -0300

    refactor of files and some msg fixes

---
+++ src/routes/usuarios/id_usuarios/id_usuarios.ts
@@ -0,0 +1,95 @@
+import type { FastifyInstance, FastifySchema } from "fastify";
+import { usuarios, usuarioPostSchema, usuarioSchema } from "../../../types/usuarios.ts";
+
+async function usuarioRoutes(fastify: FastifyInstance, options: object) {
+  fastify.get(
+    "/usuarios/:id_usuario",
+    {
+      schema: {
+        summary: "Obtener usuario por id",
+        description: "Obtener un usuario especifico según su ID",
+        tags: ["usuarios"],
+        params: {
+        type: "object",
+        properties: {
+          id_usuario: { type: "number", minimum: 1 },
+        },
+        required: ["id_usuario"],
+      },
+        response: {
+            200: usuarioSchema,
+        }
+      } as FastifySchema,
+    },
+    async function handler(req, rep) {
+        const { id_usuario } = req.params as { id_usuario: number };
+        const usuario = usuarios.find((u) => u.id_usuario === id_usuario);
+
+        return (usuario) ? usuario : rep.send({ error: "Usuario no encontrado" }).code(404);
+    }
+  );
+  fastify.put(
+    "/usuarios/:id_usuario",
+    {
+      schema: {
+        summary: "Modificar usuarios",
+        description: "Esta ruta permite modificar un nuevo usuario.",
+        tags: ["usuarios"],
+        params: {
+        type: "object",
+        properties: {
+          id_usuario: { type: "number", minimum: 1 },
+        },
+        required: ["id_usuario"],
+        },
+        body: usuarioPostSchema,
+        response: {
+            204: usuarioSchema,
+        }
+      } as FastifySchema,
+    },
+    async function handler(req, rep) {
+        const { id_usuario} = req.params as {id_usuario: number}; 
+        const { nombre, isAdmin } = req.body as {nombre: "String", isAdmin: boolean}; 
+        const usuarioIndex = usuarios.findIndex((u)=>u.id_usuario===id_usuario);
+
+        if(usuarioIndex===-1) return rep.send({ error: "Usuario no encontrado" }).code(404);
+        
+        usuarios[usuarioIndex]={nombre, isAdmin, id_usuario};
+        return usuarios[usuarioIndex];
+    }
+  );
+  fastify.delete(
+    "/usuarios/:id_usuario",
+    {
+      schema: {
+        summary: "Eliminar usuarios",
+        description: "Esta ruta permite eliminar un nuevo usuario.",
+        tags: ["usuarios"],
+        params: {
+        type: "object",
+        properties: {
+          id_usuario: { type: "number", minimum: 1 },
+        },
+        required: ["id_usuario"],
+        },
+        response: {
+            204: usuarioSchema,
+        }
+      } as FastifySchema,
+    },
+    async function handler(req, rep) {
+        const { id_usuario } = req.params as {id_usuario: number}; 
+        
+        const usuarioIndex = usuarios.findIndex((u)=>u.id_usuario===id_usuario);
+        
+        if (usuarioIndex===-1) return rep.send({ error: "Usuario no encontrado" }).code(404)
+        
+        usuarios.splice(usuarioIndex,1);
+        return rep.send("Usuario eliminado con éxito.").code(204)
+        
+    }
+  );
+}
+
+export default usuarioRoutes;
--- src/routes/usuarios/usuarios.ts
+++ src/routes/usuarios/usuarios.ts
@@ -1,38 +1,7 @@
 import type { FastifyInstance, FastifySchema } from "fastify";
+import { usuarios, usuarioPostSchema, usuarioSchema, getUltimoId, aumentarUltimoId } from "../../types/usuarios.ts";
+import type { Usuario } from "../../types/usuarios.ts";
 
-type Usuarios = {
-    nombre: string;
-    isAdmin: boolean;
-    id_usuario: number;
-};
-
-const usuarios: Usuarios[] = [
-    {  nombre: "Agustin", isAdmin: false, id_usuario: 1 },
-    {  nombre: "Matias", isAdmin: true, id_usuario: 2 },
-    {  nombre: "Brahian", isAdmin: false , id_usuario: 3 },
-];
-
-let ultimoId = usuarios[usuarios.length - 1].id_usuario;
-
-const usuarioSchema = {
-    type: "object",
-    properties: {
-        nombre: {type: "string", minLength: 2},
-        isAdmin: {type: "boolean"},
-        id_usuario : {type: "number", minimum:1},
-    },
-    required: ["nombre", "isAdmin", "id_usuario"],
-    additionalProperties: false,
-}
-const usuarioPostSchema = {
-    type: "object",
-    properties: {
-        nombre: {type: "string", minLength: 2},
-        isAdmin: {type: "boolean"},
-    },
-    required: ["nombre", "isAdmin"],
-    additionalProperties: false,
-}
 
 async function usuariosRoutes(fastify: FastifyInstance, options: object) {
   fastify.get(
@@ -46,8 +15,8 @@ async function usuariosRoutes(fastify: FastifyInstance, options: object) {
             type: "object",
             properties: {
                 nombre: {type: "string", minLength: 2},
-                // isAdmin: {type: "boolean"},
-                // id_usuario : {type: "number", minimum: 4},
+                isAdmin: {type: "boolean"},
+                id_usuario : {type: "number", minimum: 1},
             },
         },
         response: {
@@ -59,38 +28,14 @@ async function usuariosRoutes(fastify: FastifyInstance, options: object) {
       } as FastifySchema,
     },
     async function handler(req, rep) {
-        const query = req.query as {nombre:string}; 
-         if (query.nombre) return usuarios.filter((u) => u.nombre == query.nombre);
-      return usuarios;
-    }
-  );
-  fastify.get(
-    "/usuarios/:id_usuario",
-    {
-      schema: {
-        summary: "Obtener usuario por id",
-        description: "Obtener un usuario especifico según su ID",
-        tags: ["usuarios"],
-        params: {
-        type: "object",
-        properties: {
-          id_usuario: { type: "number", minimum: 1 },
-        },
-        required: ["id_usuario"],
-      },
-        response: {
-            200: usuarioSchema,
-        }
-      } as FastifySchema,
-    },
-    async function handler(req, rep) {
-        const { id_usuario } = req.params as { id_usuario: number };
-        const usuario = usuarios.find((u) => u.id_usuario === id_usuario);
-        if (usuario) {
-          return usuario;
-        } else {
-          return rep.code(404).send({ error: "Usuario no encontrado" });
-        }
+      const query = req.query as {nombre?:string, isAdmin?: boolean, id_usuario?: number};
+      let users = usuarios
+      
+      if (query.nombre)  users = users.filter((u) => u.nombre == query.nombre);
+      if (query.id_usuario) users = users.filter((u) => u.id_usuario == query.id_usuario);
+      if (query.isAdmin) users = users.filter((u) => u.isAdmin == query.isAdmin);
+
+      return (users.length !== usuarios.length) ? users : usuarios;
     }
   );
   fastify.post(
@@ -107,78 +52,15 @@ async function usuariosRoutes(fastify: FastifyInstance, options: object) {
       } as FastifySchema,
     },
     async function handler(req, rep) {
-        const { nombre, isAdmin } = req.body as Usuarios; 
-        ultimoId++
-        const usuario = {nombre, isAdmin, id_usuario: ultimoId}
-        usuarios.push(usuario);
-        rep.code(201);
+      const { nombre, isAdmin } = req.body as Usuario; 
+      aumentarUltimoId()
+
+      const usuario = {nombre, isAdmin, id_usuario: getUltimoId()}
+      usuarios.push(usuario);
+      rep.code(201);
       return usuario;
-    }
-  );
-
-
-  fastify.put(
-    "/usuarios/:id_usuario",
-    {
-      schema: {
-        summary: "Modificar usuarios",
-        description: "Esta ruta permite modificar un nuevo usuario.",
-        tags: ["usuarios"],
-        params: {
-        type: "object",
-        properties: {
-          id_usuario: { type: "number", minimum: 1 },
-        },
-        required: ["id_usuario"],
-        },
-        body: usuarioPostSchema,
-        response: {
-            204: usuarioSchema,
-        }
-      } as FastifySchema,
-    },
-    async function handler(req, rep) {
-        const { id_usuario} = req.params as {id_usuario: number}; 
-        const { nombre, isAdmin } = req.body as {nombre: "String", isAdmin: boolean}; 
-        const usuarioIndex = usuarios.findIndex((u)=>u.id_usuario===id_usuario);
-        if(usuarioIndex===-1){
-          return rep.code(404).send({error: "Usuario no encontrado."});
-        }
-        usuarios[usuarioIndex]={nombre, isAdmin, id_usuario};
-      return usuarios[usuarioIndex];
-    }
-  );
-  fastify.delete(
-    "/usuarios/:id_usuario",
-    {
-      schema: {
-        summary: "Eliminar usuarios",
-        description: "Esta ruta permite eliminar un nuevo usuario.",
-        tags: ["usuarios"],
-        params: {
-        type: "object",
-        properties: {
-          id_usuario: { type: "number", minimum: 1 },
-        },
-        required: ["id_usuario"],
-        },
-        response: {
-            204: usuarioSchema,
-        }
-      } as FastifySchema,
-    },
-    async function handler(req, rep) {
-        const { id_usuario} = req.params as {id_usuario: number}; 
-        //const { nombre, isAdmin } = req.body as {nombre: "String", isAdmin: boolean}; 
-        const usuarioIndex = usuarios.findIndex((u)=>u.id_usuario===id_usuario);
-        if (usuarioIndex===-1){
-            return rep.code(404).send({error: "Usuario no encontrado."});
-        }else{
-            usuarios.splice(usuarioIndex,1);
-            return rep.code(204).send("Usuario eliminado con éxito.");//debería mostrar mensaje pero no funciona manito
-        };
     }
   );
 }
 
-  export default usuariosRoutes;
+export default usuariosRoutes;
