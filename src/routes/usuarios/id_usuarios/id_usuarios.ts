e0221b538fa6ac76c7ca0e752610a575ce85c04f
Agustin <agustincigaran@icloud.com>
2025-09-03 18:41:06 -0300

    fix del put

--- src/routes/usuarios/id_usuarios/id_usuarios.ts
+++ src/routes/usuarios/id_usuarios/id_usuarios.ts
@@ -46,6 +46,7 @@ const usuarioRoutes:FastifyPluginAsyncTypebox= async function(fastify, options: 
       const id_body = req.body.id_usuario 
 
       if (id_usuario !== id_body) throw new PC_BadRequest("Las ids del body y pasada por parametro, no coinciden.")
+      if (typeof(isAdmin) != "boolean")  throw new PC_BadRequest("El parametro isAdmin debe ser un Boolean.")
         
       putUsuario( nombre, isAdmin, id_usuario);
       return rep.code(204).send();
